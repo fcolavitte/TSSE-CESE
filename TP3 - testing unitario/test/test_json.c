f61683d08e6e82e2a7324f8b91fc7824b647dc16
unknown <facundocolavitte@gmail.com>
2023-05-22 17:06:18 -0300

    Tests ok

--- TP3 - testing unitario/test/test_json.c
+++ TP3 - testing unitario/test/test_json.c
@@ -31,7 +31,7 @@
  *          - Buscar un "Value" del tipo String en un JSON de dos key-value con el valor buscado al comienzo
  *          - Buscar un "Value" del tipo entero en un JSON de dos key-value con el valor buscado al final
  *          - Buscar un "Value" del tipo String en un JSON de dos key-value con el valor buscado al final
- *          - Verificar que devuelva 0 si el largo máximo a analizar no alcanza para encontrarlo pero sí está en el JSON
+ *          - Verificar que devuelva 0 si el largo máximo del valor a devlver es insuficiente
  * 
  * @file   : test_json.c
  * @date   : Feb 20, 2023
@@ -42,6 +42,7 @@
 #include "unity.h"
 #include "API_Json.h"
 #include <stdint.h>
+#include <string.h>
 
 #define compilar_test_de_prueba 0
 
@@ -72,7 +73,7 @@ void test_cualquier_parametro_de_entrada_es_cero(void) {
 
     uint8_t retvalue = 1;
     retvalue = get_Json_value(0, key, value, return_max_lenght);
-	TEST_ASSERT_EQUAL_HEX8(0, retvalue);
+    TEST_ASSERT_EQUAL_HEX8(0, retvalue);
 
     retvalue = 1;
     retvalue = get_Json_value(Json, 0, value, return_max_lenght);
@@ -84,5 +85,120 @@ void test_cualquier_parametro_de_entrada_es_cero(void) {
 
     retvalue = 1;
     retvalue = get_Json_value(Json, key, value, 0);
+	TEST_ASSERT_EQUAL_HEX8(0, retvalue);
+}
+
+
+void test_key_no_encontrado(void) {
+    uint8_t* Json = "{\"key_1\":\"Value_1\"}";
+    uint8_t* key = "key_2";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(0, retvalue);
+}
+
+
+void test_encontrar_value_entero(void) {
+    uint8_t* Json = "{\"key_1\":20}";
+    uint8_t* key = "key_1";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    if(value[0]!='2'||value[1]!='0'||value[2]!='\0') {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_encontrar_value_string(void) {
+    uint8_t* Json = "{\"key_1\":\"20\"}";
+    uint8_t* key = "key_1";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    if(value[0]!='2'||value[1]!='0'||value[2]!='\0') {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_encontrar_value_entero_en_JSON_con_dos_keys(void) {
+    uint8_t* Json = "{\"key_1\":20,\"key_2\":\"prueba\"}";
+    uint8_t* key = "key_1";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    if(value[0]!='2'||value[1]!='0'||value[2]!='\0') {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_encontrar_value_string_en_JSON_con_dos_keys(void) {
+    uint8_t* Json = "{\"key_1\":\"20\",\"key_2\":\"prueba\"}";
+    uint8_t* key = "key_1";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    if(value[0]!='2'||value[1]!='0'||value[2]!='\0') {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_encontrar_value_entero_en_JSON_con_dos_keys_con_el_key_a_buscar_al_final(void) {
+    uint8_t* Json = "{\"key_1\":20,\"key_2\":35}";
+    uint8_t* key = "key_2";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    if(value[0]!='3'||value[1]!='5'||value[2]!='\0') {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_encontrar_value_string_en_JSON_con_dos_keys_con_el_key_a_buscar_al_final(void) {
+    uint8_t* Json = "{\"key_1\":\"20\",\"key_2\":\"prueba\"}";
+    uint8_t* key = "key_2";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 64;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
+	TEST_ASSERT_EQUAL_HEX8(1, retvalue);
+    uint8_t comparacion_de_strings = strcmp(value, "prueba");
+    if(0!=comparacion_de_strings) {
+        TEST_FAIL_MESSAGE("No se recibió el valor esperado");
+    }
+}
+
+
+void test_largo_maximo_a_devolver_insuficiente(void) {
+    uint8_t* Json = "{\"key_1\":1234}";
+    uint8_t* key = "key_1";
+    uint8_t value[20];
+    uint8_t return_max_lenght = 3;
+
+    uint8_t retvalue = 1;
+    retvalue = get_Json_value(Json, key, value, return_max_lenght);
 	TEST_ASSERT_EQUAL_HEX8(0, retvalue);
 }
