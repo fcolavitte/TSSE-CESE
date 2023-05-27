004f929bb0f74881f7d8135467c669154fcd3297
Facundo Colavitte <112010458+fcolavitte@users.noreply.github.com>
2023-05-27 13:23:58 -0300

    Delete TP3-testing unitario directory
    Eliminación de contenido repetido tras cambio de nombre de carpeta

--- TP3-testing unitario/build/test/dependencies/test_json.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_json.o: test/test_json.c \
- /var/lib/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- /var/lib/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h \
- src/API_Json.h
